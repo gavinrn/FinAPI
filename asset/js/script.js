ea5f0c2818281bc4e7c23da6d855a59bfd653e9a
Gavin McNutt <gavinmcnutt1@gmail.com>
2022-12-13 14:49:53 -0800

    added local storage functionality

--- asset/js/script.js
+++ asset/js/script.js
@@ -45,10 +45,14 @@ button.addEventListener("click", function () { // adds an event lister to the ad
 
 
 divStocks.addEventListener("click" , function(event) { //adds an event listner to the stocks card to save stocks to local storage on dblclick
+  event.preventDefault();
   var element = event.target;
   if (element.matches(".stocks")) {
 
-    //localStorage.setItem("symbol" , )
+    localStorage.setItem("symbol" , element.dataset.symbol);
+  }
+  else {
+
   }
 
 });
@@ -59,7 +63,7 @@ function displayToWebpage(price, symbol , color) { // displays the information f
 
   var div = document.createElement("div");
   div.setAttribute("class" , "stocks");
-  div.setAttribute("data-price" , price);
+  // div.setAttribute("data-price" , price);
   div.setAttribute("data-symbol" , symbol);
 
   div.style.backgroundColor = color;
@@ -114,10 +118,15 @@ getNewsApi(newsUrl);
 
 getNewsApi(newsUrl);
 
-getApi(teslaUrl , "limegreen");
-getApi(amazonUrl , "orangered");
-getApi(microSoft , "limegreen");
+//getApi(teslaUrl , "limegreen");
+//getApi(amazonUrl , "orangered");
+//getApi(microSoft , "limegreen");
 getApi(XOM , "orangered");
+
+if (localStorage.getItem("symbol") !== null) {
+  getApi(finnhubURL + localStorage.getItem("symbol") , "blue");
+}
+
 
 console.log(button);
 
