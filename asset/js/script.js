8e8ec717d1d4a27fa60c132d10a73a456ce5a1c3
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-12-13 13:07:53 -0800

    add space to resolve conflict

--- asset/js/script.js
+++ asset/js/script.js
@@ -23,6 +23,7 @@ const amazon = document.getElementsByTagName("button");
 const querySelector = document.body.querySelector("card-body");
 const stockSymbol = document.querySelector("#stockSymbol-input");
 
+
 const button = document.getElementById("addStocks");
 
 var i = 0;
